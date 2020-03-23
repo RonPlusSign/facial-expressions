2bd64fbdc1a33962fe3b82721bb650188268d951
Andrea Delli <andrea.delli2001@gmail.com>
2020-03-23 18:26:17 +0100

    Added responsive face-detection style
    - Added footer
    - Synchronized carousel with video
    - Displayed error if webcam is unreachable
    - Added modal to explain how face detection works

--- face-detection/detection-use.js
+++ face-detection/detection-use.js
@@ -1,47 +1,62 @@
-const video = document.getElementById('video')
+const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('./models')
-]).then(startVideo)
-
-function displayError(error) {
-  console.log(error)
-}
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models")
+]).then(startVideo);
 
 function startVideo() {
-  navigator.mediaDevices.getUserMedia({
+  navigator.mediaDevices
+    .getUserMedia({
       video: {}
-    }).then((stream) => {video.srcObject = stream})
-    .catch((error) => {
-      document.querySelector("#content-body").removeChild(document.querySelector("#video"))
-
-
-      const errorMsg = document.createElement('div')
-      errorMsg.innerHTML = "È stato riscontrato un <b>problema</b>.<br>Hai una <b>webcam</b> collegata?"
-      errorMsg.className = "alert alert-danger mt-5"
-      errorMsg.style.width = "350px"
-
-      document.querySelector("#content-body").appendChild(errorMsg)
     })
+    .then(stream => {
+      video.srcObject = stream;
+    })
+    .catch(error => {
+      document
+        .querySelector("#content-body")
+        .removeChild(document.querySelector("#video"));
+
+      const errorMsg = document.createElement("div");
+      errorMsg.innerHTML =
+        "È stato riscontrato un <b>problema</b>.<br>Hai una <b>webcam</b> collegata?";
+      errorMsg.className = "alert alert-danger mt-5";
+      errorMsg.style.width = "350px";
+
+      document.querySelector("#content-body").appendChild(errorMsg);
+    });
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+
   const displaySize = {
-    width: video.width,
-    height: video.height
-  }
-  faceapi.matchDimensions(canvas, displaySize)
+    width: video.offsetWidth,
+    height: video.offsetHeight
+  };
+
+  faceapi.matchDimensions(canvas, displaySize);
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    // console.log(detections)
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+    canvas.getBoundingClientRect().top = canvas.getBoundingClientRect().top;
+    canvas.getBoundingClientRect().left = video.getBoundingClientRect().left;
+
+    document.body.append(canvas);
+  }, 100);
+});

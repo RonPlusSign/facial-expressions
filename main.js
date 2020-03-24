c6bac37394c7b887628d481a5bccede32fa294b8
Andrea Delli <andrea.delli2001@gmail.com>
2020-03-24 16:44:59 +0100

    Revert "Started to change carousel content to table"
    This reverts commit 70bff03b3ba62fc145e17c2d9cef474fe2f9a541.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import modules from "./modules/all.mjs";
 
 // ----------------- CARDS -----------------
-const cards = [];
 
 /*
 Card structure:
@@ -22,12 +21,12 @@ Card structure:
 function populateBody() {
 
   // Append a hidden card before the element
-  // $("#index-carousel").append("<div class='card my-card hidden' style='cursor: pointer; pointer-events: none; border: 0;'></div>");
+  $("#index-carousel").append("<div class='card my-card hidden' style='cursor: pointer; pointer-events: none; border: 0;'></div>");
 
   // Append cards after the element
   modules.forEach(element => {
-    cards.push(`<div class="card my-card">
-        <img class="card-img-top" src="${element.angry().imageUrl}">
+    $("#index-carousel").append(`<div class="card my-card">
+        <img class="card-img-top" src="${element.angry().imageUrl}" />
         <div class="card-body">
           <h5 class="card-title">${element.angry().title}</h5>
           <p class="card-text">${element.angry().description}</p>
@@ -36,102 +35,51 @@ function populateBody() {
       </div>`);
   });
 
-  // console.log(cards)
-
   // Append a hidden card after the element
-  // $("#index-carousel").append("<div class='card my-card hidden' style='cursor: pointer; pointer-events: none; border: 0;'></div>");
+  $("#index-carousel").append("<div class='card my-card hidden' style='cursor: pointer; pointer-events: none; border: 0;'></div>");
 }
 
 populateBody();
 
 // ----------------- Carousel logic -----------------
-var num = cards.length;
-var even = num / 2
-var odd = (num + 1) / 2
+var num = $(".my-card").length;
+var even = num / 2;
+var odd = (num + 1) / 2;
 
 if (num % 2 == 0) {
-  $("#index-carousel td.active").append(cards[even]);
-  $("#index-carousel td.active .my-card").addClass("active");
-
-  $("#index-carousel td.prev").append(cards[even - 1]);
-  $("#index-carousel td.prev .my-card").removeClass("active");
-  $("#index-carousel td.prev .my-card").addClass("prev");
-
-  $("#index-carousel td.next").append(cards[even + 1]);
-  $("#index-carousel td.next .my-card").removeClass("active");
-  $("#index-carousel td.next .my-card").addClass("next");
+  $(".my-card:nth-child(" + even + ")").addClass("active");
+  $(".my-card:nth-child(" + even + ")")
+    .prev()
+    .addClass("prev");
+  $(".my-card:nth-child(" + even + ")")
+    .next()
+    .addClass("next");
 } else {
-  $("#index-carousel td.active").append(cards[odd]);
-  $("#index-carousel td.active .my-card").addClass("active");
-
-  $("#index-carousel td.prev").append(cards[odd - 1]);
-  $("#index-carousel td.prev .my-card").removeClass("active");
-  $("#index-carousel td.prev .my-card").addClass("prev");
-
-  $("#index-carousel td.next").append(cards[odd + 1]);
-  $("#index-carousel td.next .my-card").removeClass("active");
-  $("#index-carousel td.next .my-card").addClass("next");
+  $(".my-card:nth-child(" + odd + ")").addClass("active");
+  $(".my-card:nth-child(" + odd + ")")
+    .prev()
+    .addClass("prev");
+  $(".my-card:nth-child(" + odd + ")")
+    .next()
+    .addClass("next");
 }
 
 // ---- Carousel events ----
 
 $(".my-card").click(function () {
-  // Remove all classes
-  $(this).removeClass("prev active next");
-
+  $(this).removeClass("prev next");
   $(this)
-    .parent()
     .siblings()
-    .children()
     .removeClass("prev active next");
 
-  // Add next element
-  if ($(this) // If the element has no next, add it from the cards array
-    .parent()
-    .next()
-    .children()[0] == undefined) {
-
-    cards.forEach(element => {
-      if (element == getStringFromNode($(this)[0]))
-      $("#index-carousel td.next").innerHTML = element;
-    })
-  } else $(this)
-    .parent()
-    .next()
-    .children()
-    .addClass("next");
-
-  // Add prev element
-  if ($(this) // If the element has no next, add it from the cards array
-    .parent()
-    .prev()
-    .children()[0] == undefined) {
-
-  } else $(this)
-    .parent()
-    .next()
-    .children()
-    .addClass("prev");
-
-  // Add active class
   $(this).addClass("active");
+  $(this)
+    .prev()
+    .addClass("prev");
+  $(this)
+    .next()
+    .addClass("next");
 });
-
-var getStringFromNode = (function() {
-  var DIV = document.createElement("div");
-
-  if ('outerHTML' in DIV)
-    return function(node) {
-      return node.outerHTML;
-    };
-
-  return function(node) {
-    var div = DIV.cloneNode();
-    div.appendChild(node.cloneNode(true));
-    return div.innerHTML;
-  };
-
-})();
 
 // Keyboard carousel navigation 
 

57e6c7e17a80673294b79996f4cffd62376be5c0
Andrea Delli <andrea.delli2001@gmail.com>
2020-03-26 11:00:21 +0100

    Fixed typo

--- modules/all.mjs
+++ modules/all.mjs
@@ -3,7 +3,7 @@ import { Rabbia } from "./emozioni/rabbia.mjs";
 import { Tristezza } from "./emozioni/tristezza.mjs";
 import { Stupore } from "./emozioni/stupore.mjs";
 import { Disgusto } from "./emozioni/disgusto.mjs";
-import { Paura } from "./emozioni/Paura.mjs";
+import { Paura } from "./emozioni/paura.mjs";
 
 
 export default [Felicità, Rabbia, Tristezza, Stupore, Disgusto, Paura];
